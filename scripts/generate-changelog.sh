5061061a3b03117a66b049f4dbfaa66a70d63a64
xingoxu <xingoxu@gmail.com>
2020-09-23 12:23:33 +0900

    fix: fix some Changelog CI detail

---
+++ scripts/generate-changelog.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo ${GITHUB_REPOSITORY}
+
+git checkout ${GITHUB_HEAD_REF}
+
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
+
+npm run generate-changelog
+
+git add -A
+git commit -m "(Changelog CI) Added Changelog"
+git push -u origin ${GITHUB_HEAD_REF}
